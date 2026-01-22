a261b8b4a7bf74cc706927891bd03d8cd351ea95
julianoeni20 <julianoeni20@gmail.com>
2026-01-22 17:51:07 +0000

    asdasd

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 import streamlit as st
 from plots import us_treasury_plots
 
-st.title("FBU Macro Dashboard")
+def check_password():
+    """Returns `True` if the user had the correct password."""
+    def password_entered():
+        if st.session_state["password"] == st.secrets["PASSWORD"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store password
+        else:
+            st.session_state["password_correct"] = False
 
-with st.spinner("Fetching latest market data..."):
-    fig_ts, fig_curve = us_treasury_plots()
+    if "password_correct" not in st.session_state:
+        # First run, show input for password
+        st.text_input("Password", type="password", on_change=password_entered, key="password")
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error
+        st.text_input("Password", type="password", on_change=password_entered, key="password")
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct
+        return True
 
-st.plotly_chart(fig_ts)
-st.plotly_chart(fig_curve)
+if check_password():
+    
+    st.title("FBU Macro Dashboard")
+
+    with st.spinner("Fetching latest market data..."):
+        fig_ts, fig_curve = us_treasury_plots()
+
+    st.plotly_chart(fig_ts)
+    st.plotly_chart(fig_curve)
